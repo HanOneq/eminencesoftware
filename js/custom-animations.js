04051793423b5e77d1e9c2079ec1326eadbd6bad
vishal <vanbeek56@gmail.com>
2016-12-03 17:01:55 -0500

    Initial commit

---
+++ js/custom-animations.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+    
+    //setTimeout(addIntroLeadInAnimation, 200);
+    
+});
+
+function addIntroLeadInAnimation(){
+    jQuery('.intro-lead-in').addClass('animated fadeInLeft');
+}
---
+++ js/header-bg.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function () {
+
+    var img = [
+        'http://localhost/justanotheritcompany/img/skyscrapers.jpg',
+        //'http://localhost/justanotheritcompany/img/nyc-street.jpg',
+        //'http://localhost/justanotheritcompany/img/nyc-taxis.jpg',
+        //'http://localhost/justanotheritcompany/img/skyscraper-top.jpg',
+    ];
+    var imgCount = img.length;
+    //console.log(imgCount);
+    
+    var val = Math.floor(Math.random() * imgCount);
+
+    $('header').css('background-image', 'url(' + img[val] + ')');
+});
