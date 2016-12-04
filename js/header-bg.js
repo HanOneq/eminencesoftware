0a98b149009d783ef5ad001d04416a2873d38745
vishal <vanbeek56@gmail.com>
2016-12-04 17:29:38 -0500

    making updates

--- js/header-bg.js
+++ js/header-bg.js
@@ -2,9 +2,9 @@ jQuery(document).ready(function () {
 
     var img = [
         'http://localhost/justanotheritcompany/img/skyscrapers.jpg',
-        //'http://localhost/justanotheritcompany/img/nyc-street.jpg',
-        //'http://localhost/justanotheritcompany/img/nyc-taxis.jpg',
-        //'http://localhost/justanotheritcompany/img/skyscraper-top.jpg',
+        'http://localhost/justanotheritcompany/img/nyc-street.jpg',
+        'http://localhost/justanotheritcompany/img/nyc-taxis.jpg',
+//        'http://localhost/justanotheritcompany/img/skyscraper-top.jpg',
     ];
     var imgCount = img.length;
     //console.log(imgCount);
