2e68cfa42114c717f18e69a306110ba375e07b0b
vishal <vanbeek56@gmail.com>
2016-12-05 07:52:56 -0500

    updates

--- index.php
+++ index.php
@@ -142,12 +142,12 @@
                 <div class="row">
                     <div class="col-lg-12 text-center">
                         <h2 class="section-heading">Portfolio</h2>
-                        <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                        <h3 class="section-subheading text-muted">A few of our past projects</h3>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-4 col-sm-6 portfolio-item">
-                        <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+                        <a href="#portfolioModal1" class="portfolio-link" data-toggle="">
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content">
                                     <i class="fa fa-plus fa-3x"></i>
@@ -156,12 +156,12 @@
                             <img src="img/portfolio/roundicons.png" class="img-responsive" alt="">
                         </a>
                         <div class="portfolio-caption">
-                            <h4>Round Icons</h4>
-                            <p class="text-muted">Graphic Design</p>
+                            <h4>Essence.com</h4>
+                            <p class="text-muted">Drupal Development</p>
                         </div>
                     </div>
                     <div class="col-md-4 col-sm-6 portfolio-item">
-                        <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+                        <a href="#portfolioModal2" class="portfolio-link" data-toggle="">
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content">
                                     <i class="fa fa-plus fa-3x"></i>
@@ -170,12 +170,12 @@
                             <img src="img/portfolio/startup-framework.png" class="img-responsive" alt="">
                         </a>
                         <div class="portfolio-caption">
-                            <h4>Startup Framework</h4>
+                            <h4>Menshealth.com</h4>
                             <p class="text-muted">Website Design</p>
                         </div>
                     </div>
                     <div class="col-md-4 col-sm-6 portfolio-item">
-                        <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+                        <a href="#portfolioModal3" class="portfolio-link" data-toggle="">
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content">
                                     <i class="fa fa-plus fa-3x"></i>
@@ -184,8 +184,8 @@
                             <img src="img/portfolio/treehouse.png" class="img-responsive" alt="">
                         </a>
                         <div class="portfolio-caption">
-                            <h4>Treehouse</h4>
-                            <p class="text-muted">Website Design</p>
+                            <h4>PeopleEnEspanol.com</h4>
+                            <p class="text-muted">Drupal Development</p>
                         </div>
                     </div>
                 </div>
