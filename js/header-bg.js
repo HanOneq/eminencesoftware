a853d87ce1bd776eae4bbdf977ba2025bc9b1b1a
vishal <vanbeek56@gmail.com>
2016-12-06 17:15:46 -0500

    updates

--- js/header-bg.js
+++ js/header-bg.js
@@ -1,13 +1,28 @@
 jQuery(document).ready(function () {
-    var website = window.location.href;
+    var website = null;
+    console.log(website);
+
+
+    //location.protocol + '//' + location.host + location.pathname
+    if (window.location.host == 'localhost') {
+        website = window.location.protocol + '//' + window.location.host + '/' + window.location.pathname;
+        console.log('you are local at: '+ website);
+    }
+    else{
+        website = window.location.protocol + '//' + window.location.host;
+        console.log('you are not local at: ' + website);
+    }
+    //console.log(location.protocol + '/' + window.location.host + '/' + window.location.pathname);
+
+
     var img = [
         'img/skyscrapers.jpg',
         'img/nyc-street.jpg',
         'img/nyc-taxis.jpg',
-//        '/img/skyscraper-top.jpg',
+        //'/img/skyscraper-top.jpg',
     ];
     var imgCount = img.length;
-    //console.log(imgCount);
+    console.log(imgCount);
 
     var val = Math.floor(Math.random() * imgCount);
 
