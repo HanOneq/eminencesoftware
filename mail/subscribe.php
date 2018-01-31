3e83fd9ad53f2a5092ae76ce37ae9807536907d6
Vanbeek <vanbeek@eminencesoftware.com>
2018-01-31 13:02:03 -0500

    updating email for subscribe.php and contact_me.php

--- mail/subscribe.php
+++ mail/subscribe.php
@@ -3,10 +3,10 @@
 $email = $_POST['email'];
 
 // create email body and send it	
-$to = 'sales@eminence.us';
+$to = 'admin@eminencesoftware.com';
 $email_subject = "Website Subscription Form";
 $email_body = "You've received a new message request to subscribe to your website.\n\n"."Here are the details:\n\nEmail: $email\n\nWould like to be added to the subscription list.";
-$headers = "From: sales@eminence.us\n";
+$headers = "From: admin@eminencesoftware.com\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;
